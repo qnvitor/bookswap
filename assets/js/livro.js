f68c2f138914c9861043edaf391126ef4bfb9579
Vitor Queiroz <vitornqueiroz.dir@gmail.com>
2024-04-18 23:24:37 -0300

    feature camera

--- assets/js/livro.js
+++ assets/js/livro.js
@@ -1,10 +1,11 @@
+// Câmera
 var video = document.getElementById('preview');
 var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
 var captureButton = document.getElementById('capture');
 
 captureButton.addEventListener('click', function() {
-  navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } }) // Câmera traseira
+  navigator.mediaDevices.getUserMedia({ video: {facingMode: 'environment'}})
     .then(function(stream) {
       video.srcObject = stream;
       video.style.display = 'block';
@@ -12,6 +13,10 @@ captureButton.addEventListener('click', function() {
         video.play();
         context.drawImage(video, 0, 0, canvas.width, canvas.height);
         // Aqui você pode fazer o que quiser com a imagem capturada
+            // Exibir a imagem capturada
+    const capturedImage = document.getElementById('canvas');
+    capturedImage.src = canvas.toDataURL();
+    capturedImage.style.display = 'block';
       };
     })
     .catch(function(error) {
